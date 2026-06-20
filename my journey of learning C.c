b3078eb2ab0d753db9822d16cf1e8c6a0ad10897
lebaochaucao-ui <lebaochaucao@gmail.com>
2026-06-20 15:22:29 +0700

    day 1
    i wrote this on my first day of learning C :)

---
+++ my journey of learning C.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+                                            //day 2 learning C!!, ig...
+                                            //today is 20/6/2026
+                                            //this is my first C program,about market
+                                            //i wrote this when i'm 12 years old
+
+int main() {
+    char ten[10];
+    int soluong;
+    int giatien;
+    int sotiencantra;
+    int sotienanchan;
+
+    printf("what do you even want to pour in your mouth? \n");
+    scanf("%s", ten);
+    printf("idk what is %s \n", ten);
+
+    printf("what's the price of %s ? \n", ten);
+    scanf("%d", &giatien);
+    printf("are you a thief?\n");
+
+    printf("how much %s do you want to buy? \n", ten);
+    scanf("%d", &soluong);
+    printf("you're so fat\n");
+
+    sotiencantra = soluong * giatien;
+    sotienanchan = sotiencantra * 30;
+
+    printf("so if you want to buy %d %s, it'll be %d vnd, give me %d vnd right now.\n", soluong, ten, sotiencantra, sotienanchan);
+
+
+    return 0;
+}
