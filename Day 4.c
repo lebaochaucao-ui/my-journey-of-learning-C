a3bd341d83500bc2fb2f20c5d1b2973e7985348a
Natsuki <lebaochaucao@gmail.com>
2026-06-24 10:54:52 +0700

    ;-;
    -_-

---
+++ Day 4.c
@@ -0,0 +1,49 @@
+//hi
+//i'm sorry for not posting my work on github at day 4,but i'll take today's program as day 4!
+//today is 24th june 2026
+//today's program is about ascii!
+//i just learned that.
+
+
+//XÓA DẤU CHÚ THÍCH ĐI ĐỂ CHẠY VÀ CHÚ THÍCH TẤT CẢ NHỮNG DÒNG  CODE KHÁC
+
+#include <stdio.h>
+// day la chuong trinh ghi chu in hoa -> in thuong
+//int main(){
+    //char chuhoa;
+    //char chuthuong;
+    //scanf("%c", &chuhoa);
+    //chuthuong = chuhoa + 32;
+    //printf("%c", chuthuong);
+//}
+
+//day la chuong trinh ghi chu in thuong -> in hoa
+
+//int main(){
+    //char inhoa;
+    //char inthuong;
+    //scanf("%c", &inthuong);
+    //inhoa = inthuong - 32;
+    //printf("%c", inhoa);
+
+//}
+
+//day la chuong trinh ghi chu -> bang ma ASCII
+
+//int main(){
+    //char chu;
+    //int ASCII;
+    //scanf("%c", &chu);
+    //ASCII = chu * 1;
+    //printf("%d", ASCII);
+//}
+
+//day la chuong trinh ghi so trong bang ma ASCII -> chu
+
+int main(){
+    char kytu;
+    int ascii;
+    scanf("%d", &ascii);
+    kytu = ascii;
+    printf("%c", kytu);
+}
