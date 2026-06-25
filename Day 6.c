0b9e8d616915f22109c6849ef91dc917528ad90d
Natsuki <lebaochaucao@gmail.com>
2026-06-25 15:31:17 +0700

    just practice...............................
    Shikkei

---
+++ Day 6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//practice for loop system.............
+//today is 25th June 2026
+
+int main(){
+    for(int i = 0 ; i <= 9244 ; i++){
+        printf("i love you %d ", i);
+    }
+}
