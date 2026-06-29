4e1187de3c4773d461385bdf4fa8a0f018e85594
Natsuki <lebaochaucao@gmail.com>
2026-06-29 09:34:54 +0700

    Add files via upload

---
+++ Day 9.c
@@ -0,0 +1,22 @@
+//bu cho ngay 9
+//today is 29th june 2026
+//chuong trinh tinh tong day so
+
+#include <stdio.h>
+
+int main(){
+    int num;
+    int sum;
+
+    printf("give me a number vro \n");
+    scanf("%d", &num);
+        for (int i = 1 ; i <= num ; i++){
+            sum = sum + i;
+
+
+        }
+
+        printf("%d \n", sum);
+
+
+}
